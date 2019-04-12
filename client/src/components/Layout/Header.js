76c7056cf91eed3b0c776e63694cd23aefa67938
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-04-12 12:41:33 -0500

    added components

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../../assets/styles.css";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Header</h1>
+            </div>
+        )
+    }
+}
