04632d458a54d03ae405816be63aaf9a2a1dbdbb
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-04-27 23:00:15 -0500

    notes

--- client/src/components/Layout/BodyContainer.js
+++ client/src/components/Layout/BodyContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import Content from "../Content/Content"
 
 // body container contains the contents of the page
+// body container custom css import
 
 export default class BodyContainer extends Component {
 render() {
