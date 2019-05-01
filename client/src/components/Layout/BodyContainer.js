67219fab9aa35f345e100e36b5aade7dae50c007
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-05-01 16:00:40 -0500

    update

--- client/src/components/Layout/BodyContainer.js
+++ client/src/components/Layout/BodyContainer.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react"
 import Content from "../Content/Content"
 
-// body container contains the contents of the page
-// body container custom css import
-
 export default class BodyContainer extends Component {
 render() {
     return (
